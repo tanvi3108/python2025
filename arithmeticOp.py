f0c473772fe3ba8395ce955c3f5cf596102ed334
tanvi3108 <haldauntanvi@gmail.com>
2025-07-18 17:17:00 +0800

    arithmetic

---
+++ arithmeticOp.py
@@ -0,0 +1,11 @@
+# Simple Hello Program
+print("Hello, Student! Let's see the arithmetic operations")
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+print("a+b= ",a+b)
+print("a-b= ",a-b)
+print("a*b= ",a*b)
+print("a**b= ",a**b)#exponentiation(raises one number to the power of another)
+print("a/b= ",a/b)
+print("a//b= ",a//b)#floor division(also caled INTEGER DIVISION, removes the decimal part after dividing two numbers)
+print("a%b= ",a%b)
--- first.py
+++ first.py
@@ -1,2 +1,36 @@
-# Simple Hello Program
-print("Hello, Student!")
+print('hello')
+#print(''hello'')#wrong syntax
+print('''hello''')
+print("hello")
+print("""hello""")
+#print(""hello"")#wrong syntax
+print("'hello'")
+print('"hello"')
+
+#variables
+name='Tanvi'
+name2="Tanvi"
+#name3=''Tanvi''
+#name4=""Tanvi""
+name5='''Tanvi'''
+name6="""Tanvi"""
+
+a=34
+b=36.7
+abc=True
+xyz=None
+y=3+7j
+print("the type of data: ",type(name))
+print(type(name2))
+# print(type(name3))
+# print(type(name4))
+print(type(name5))
+print(type(name6))
+print(type(a))
+print(type(b))
+print(type(abc))
+print(type(xyz))
+print(type(y))
+
+
+
